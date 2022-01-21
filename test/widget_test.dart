750358cf257dff7ca4fa1cd05fc86edeb706b4c4
Your Name <you@example.com>
2022-01-21 10:57:46 +0530

    Updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:thirvusoft_app/views/screens/main.dart';
+import 'package:thirvusoft_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
